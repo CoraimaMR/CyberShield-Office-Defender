1473561ad84950f4749b59c86453c45f25b83d6a
cmerrod1503 <coraimamr1503@gmail.com>
2026-02-18 11:46:09 +0100

    Add popup feedback UI for email answers
    Add popup window assets and import settings (yess/noo/popup images) and update the email_level scene to include a feedback popup with correct/incorrect labels, description text, and a close button (signal connected). Update email_level.gd to manage the popup: new onready refs, initialize popup hidden, populate the educational tip, show the popup on answer validation (showing correct or incorrect state), and disable/reenable Trust/Report buttons while the popup is visible.

--- scripts/email_level.gd
+++ scripts/email_level.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Control # EMAIL LEVEL
 
 # This array will hold your EmailResource files (.tres)
 # You can drag and drop them into the Inspector
@@ -12,8 +12,13 @@ var current_index: int = 0
 @onready var body_label = %BodyLabel
 @onready var incorrect_sound = $incorrect
 @onready var correct_sound = $correct
+@onready var tip_label = $"popup window/description"
+@onready var popup_window = $"popup window"
+@onready var correct_label = $"popup window/text correct"
+@onready var incorrect_label = $"popup window/text incorrect"
 
 func _ready():
+	popup_window.visible = false
 	# Update the UI with the first email if the list is not empty
 	if email_list.size() > 0:
 		display_email()
@@ -35,6 +40,7 @@ func display_email():
 	var mail = email_list[current_index]
 	sender_label.text = "From: " + mail.sender_name + " <" + mail.sender_email + ">"
 	subject_label.text = "Subject: " + mail.subject
+	tip_label.text = mail.educational_tip
 	
 	# We use 'text' for RichTextLabel, or 'set_bbcode' in older versions
 	body_label.text = mail.body_text
@@ -52,11 +58,13 @@ func validate_choice(user_said_phishing: bool):
 		print("Correct! +5 points")
 		Autoload.add_points(5)
 		correct_sound.play()
+		window(true)
 	else:
 		print("Wrong! -10 points")
 		Autoload.remove_points(10)
 		Autoload.remove_lifes(1)
 		incorrect_sound.play()
+		window(false)
 	
 	# Move to the next email
 	current_index += 1
@@ -67,3 +75,19 @@ func validate_choice(user_said_phishing: bool):
 		# WIN MENU
 		Autoload.save_scene() # Save scene
 		get_tree().call_deferred("change_scene_to_file", "res://scenes/win_menu.tscn") # Go to the win menu
+
+func window(correcto: bool):
+	popup_window.visible = true
+	%TrustButton.disabled = true
+	%ReportButton.disabled = true
+	if correcto:
+		correct_label.visible = true
+		incorrect_label.visible = false
+	else:
+		incorrect_label.visible = true
+		correct_label.visible = false
+
+func _on_button_pressed() -> void:
+	popup_window.visible = false
+	%TrustButton.disabled = false
+	%ReportButton.disabled = false
